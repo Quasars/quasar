d3fef52e52f420f9270eed6924b93820bfc962e1
Marko Toplak <marko.toplak@gmail.com>
2018-10-25 14:19:21 +0200

    splash screen

--- quasar/launcher/__init__.py
+++ quasar/launcher/__init__.py
@@ -1,11 +1,14 @@
 import quasar
+from quasar.launcher.splash import splash_screen
 quasar.__version__ = "0.0.1"
+
 
 class Launcher:
     def launch(self):
         self.fix_application_name()
         self.fix_application_dirs()
         self.replace_update_check()
+        self.replace_splash_screen()
 
         self.main()
 
@@ -77,6 +80,22 @@ class Launcher:
                 return base
 
         environ.cache_dir = cache_dir
+
+    def replace_splash_screen(self):
+        from AnyQt.QtCore import Qt
+        from AnyQt.QtGui import QColor
+        from Orange.canvas import config
+        from Orange.canvas.gui.splashscreen import SplashScreen
+
+        config.splash_screen = splash_screen
+
+        sm = SplashScreen.showMessage
+
+        # showing currently loading widget
+        def showMessage(self, message, alignment=Qt.AlignLeft, color=Qt.black):
+            sm(self, message, alignment=alignment, color=QColor("#ff5900"))
+
+        SplashScreen.showMessage = showMessage
 
     def replace_update_check(self):
         from Orange.canvas import __main__
---
+++ quasar/launcher/splash.py
@@ -0,0 +1,29 @@
+import pkg_resources
+from PyQt5.QtCore import QCoreApplication, QPoint, Qt, QRect
+from PyQt5.QtGui import QPixmap, QFont, QFontMetrics, QPainter, QColor
+
+
+def splash_screen():
+    """
+    """
+    pm = QPixmap(
+        pkg_resources.resource_filename(
+            __name__, "icons/splash.png")
+    )
+
+    version = QCoreApplication.applicationVersion()
+    size = 26 if len(version) < 6 else 16
+    font = QFont("Arial")
+    font.setPixelSize(size)
+    metrics = QFontMetrics(font)
+    br = metrics.boundingRect(version).adjusted(-5, 0, 5, 0)
+    br.moveBottomLeft(QPoint(468, 131))
+
+    p = QPainter(pm)
+    p.setRenderHint(QPainter.Antialiasing)
+    p.setRenderHint(QPainter.TextAntialiasing)
+    p.setFont(font)
+    p.setPen(QColor("#ff5900"))
+    p.drawText(br, Qt.AlignCenter, version)
+    p.end()
+    return pm, QRect(188, 155, 250, 328)
