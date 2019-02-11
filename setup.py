93118a3c1785fee6707649137567cb9e17401ccc
Marko Toplak <marko.toplak@gmail.com>
2019-02-11 14:40:55 +0100

    Version 0.3.0
    Orange>=3.20.0 and orange-spectroscopy>=0.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages, Command
 
 NAME = "Quasar"
 
-VERSION = "0.2.0"
+VERSION = "0.3.0"
 
 DESCRIPTION = "Quasar is a collection of data analysis toolboxes extending the Orange suite."
 LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README.pypi')).read()
