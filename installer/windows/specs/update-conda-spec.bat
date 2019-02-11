e4db4de781b1faf7272eed4df103c0167b97b29a
Marko Toplak <marko.toplak@gmail.com>
2019-02-11 14:46:14 +0100

    Windows: update conda spec

--- installer/windows/specs/update-conda-spec.bat
+++ installer/windows/specs/update-conda-spec.bat
@@ -1,5 +1,8 @@
-conda config --add channels conda-forge
-conda remove -y -n orange --all
+call conda config --add channels conda-forge
+call conda remove -y -n orange --all
 REM added some spectroscopy dependencies by hand as it is packaged manually
-conda create -y -n orange python=3.6.* orange3=3.18.0 keyring scipy=1.1.0 numpy=1.15.4 scikit-learn=0.20.1 spectral>=0.18 colorcet h5py
-conda list -n orange --export --explicit --md5 > conda-spec.txt
+call conda create -y -n orange python=3.6.* orange3=3.20.0 ^
+ keyring scipy=1.1.* numpy=1.15.* pyqt=5.6.* ^
+ scikit-learn=0.20.* spectral ^
+ colorcet h5py
+call conda list -n orange --export --explicit --md5 > conda-spec.txt
