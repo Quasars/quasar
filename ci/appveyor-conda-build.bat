9b8c0ac0ed316982373718288aa74199783ac844
Marko Toplak <marko.toplak@gmail.com>
2019-04-30 22:09:14 +0200

    .

--- ci/appveyor-conda-build.bat
+++ ci/appveyor-conda-build.bat
@@ -14,26 +14,7 @@ if  "%PLATTAG%" == "" (
 "%CONDA%" config --append channels conda-forge  || exit /b !ERRORLEVEL!
 
 if not "%BUILD_LOCAL%" == "" (
-    "%CONDA%" install --yes conda-build=3.17.8  || exit /b !ERRORLEVEL!
-    "%CONDA%" build --python %PYTHON_VERSION% --no-test ../specs/conda-recipe ^
-        || exit /b !ERRORLEVEL!
-
-    rem # Copy the build conda pkg to artifacts dir
-    rem # and the cache\conda-pkgs which is used later by build-conda-installer
-    rem # script
-
-    mkdir ..\conda-pkgs        || exit /b !ERRORLEVEL!
-    mkdir ..\cache             || exit /b !ERRORLEVEL!
-    mkdir ..\cache\conda-pkgs  || exit /b !ERRORLEVEL!
-
-    for /f %%s in ( '"%CONDA%" build --output --python %PYTHON_VERSION% ../specs/conda-recipe' ) do (
-        copy /Y "%%s" ..\conda-pkgs\  || exit /b !ERRORLEVEL!
-        copy /Y "%%s" ..\cache\conda-pkgs\  || exit /b !ERRORLEVEL!
-    )
-
-    for /f %%s in ( '"%PYTHON%" setup.py --version' ) do (
-        set "VERSION=%%s"
-    )
+    rem # Disabled local builds
 ) else (
     set "VERSION=%BUILD_COMMIT%"
 )
@@ -41,23 +22,13 @@ if not "%BUILD_LOCAL%" == "" (
 echo VERSION = %VERSION%
 
 if "%CONDA_SPEC_FILE%" == "" (
-    "%CONDA%" create -n env --yes --use-local ^
-                 python=%PYTHON_VERSION% ^
-                 numpy=1.14.* ^
-                 scipy=1.1.* ^
-                 scikit-learn=0.20.* ^
-                 bottleneck=1.2.* ^
-                 pyqt=5.6.* ^
-                 Orange3=%VERSION% ^
-        || exit /b !ERRORLEVEL!
-
-    "%CONDA%" list -n env --export --explicit --md5 > env-spec.txt
+    call installer/windows/specs/update-conda-spec.bat || exit /b !ERRORLEVEL!
     set CONDA_SPEC_FILE=env-spec.txt
 )
 
 type "%CONDA_SPEC_FILE%"
 
-bash -e scripts/windows/build-conda-installer.sh ^
+bash -e installer/windows/build-conda-installer.sh ^
         --platform %PLATTAG% ^
         --cache-dir ../cache ^
         --dist-dir dist ^
@@ -66,7 +37,7 @@ bash -e scripts/windows/build-conda-installer.sh ^
     || exit /b !ERRORLEVEL!
 
 
-for %%s in ( dist/Orange3-*Miniconda*.exe ) do (
+for %%s in ( dist/Quasar-*Miniconda*.exe ) do (
     set "INSTALLER=%%s"
 )
 
--- installer/windows/build-and-sign.sh
+++ installer/windows/build-and-sign.sh
@@ -3,47 +3,11 @@
 set -e
 
 INSTALLER="$( cd "$(dirname "$0")/.." ; pwd -P )"
-BUILD="$INSTALLER/windows/build"
 BASE="$( cd "$(dirname "$0")/../.." ; pwd -P )"
 DIST="$BASE/dist"
-OPUSFC="/home/marko/conda-opusfc"
-SPECTROSCOPY_BRANCH="0.4.1"
-
-# Build updated conda package
-mkdir -p "$BUILD/conda"
-rm -rf "$BUILD/conda"/*
-
-rm -rf "$BUILD/spectroscopy"
-
-git clone --single-branch --branch "$SPECTROSCOPY_BRANCH" \
-   https://github.com/Quasars/orange-spectroscopy \
-   "$BUILD/spectroscopy"
-
-# Manually build orange-spectroscopy that is in the same folder as quasar
-conda build "$BUILD/spectroscopy/conda" \
-  --output-folder "$BUILD/conda"
-
-conda build "$INSTALLER/conda" \
-  --output-folder "$BUILD/conda"
 
 # Build new conda package
-NEW_SPEC="$BUILD/conda/conda-spec.txt"
-sed '/^file/ d' \
-  "$INSTALLER/windows/specs/conda-spec.txt" \
-  > $NEW_SPEC
-echo "" >> $NEW_SPEC
-CONDA_PACKAGE=$( find "$BUILD/conda" \
-  -name "quasar*bz2" \
-  -exec echo "file://{}" \; )
-echo "$CONDA_PACKAGE" >> $NEW_SPEC
-CONDA_PACKAGE=$( find "$BUILD/conda" \
-  -name "orange-spectroscopy*bz2" \
-  -exec echo "file://{}" \; )
-echo "$CONDA_PACKAGE" >> $NEW_SPEC
-CONDA_PACKAGE=$( find "$OPUSFC" \
-  -name "opusfc*py36*bz2" \
-  -exec echo "file://{}" \; )
-echo "$CONDA_PACKAGE" >> $NEW_SPEC
+NEW_SPEC="$INSTALLER/windows/specs/conda-spec.txt"
 
 # Build new installer
 ./build-conda-installer.sh \
