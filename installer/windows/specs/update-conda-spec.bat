f43ff84ddb13a5661f78bd717e4ddd493d082f23
Marko Toplak <marko.toplak@gmail.com>
2019-05-02 16:06:00 +0200

    Add extranormal3 to windows build

--- installer/windows/specs/update-conda-spec.bat
+++ installer/windows/specs/update-conda-spec.bat
@@ -11,7 +11,7 @@ rem %CONDA% remove -y -n orange --all
 
 rem added some spectroscopy dependencies by hand as it is packaged manually
 "%CONDA%" create -y -n env python=3.6.* orange3=3.20.1 ^
- quasar=0.3.1 orange-spectroscopy=0.4.3 opusFC=1.2.* ^
+ quasar=0.3.1 orange-spectroscopy=0.4.3 opusFC=1.2.* extranormal3=0.0.3 ^
  keyring scipy=1.1.* numpy=1.15.* pyqt=5.6.* ^
  scikit-learn=0.20.* spectral ^
  colorcet h5py || exit /b !ERRORLEVEL!
