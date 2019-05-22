7b4256c543ba198f9b0a1c0bd473abf6233e6b70
Marko Toplak <marko.toplak@gmail.com>
2019-05-22 22:53:42 +0200

    Release 0.4.0
    Orange 3.21.0, Orange-Spectroscopy 0.4.4

--- installer/windows/specs/update-conda-spec.bat
+++ installer/windows/specs/update-conda-spec.bat
@@ -9,9 +9,8 @@ if "%CONDA%" == "" (
 
 rem %CONDA% remove -y -n orange --all
 
-rem added some spectroscopy dependencies by hand as it is packaged manually
-"%CONDA%" create -y -n env python=3.6.* orange3=3.20.1 ^
- quasar=0.3.2 orange-spectroscopy=0.4.4 opusFC=1.2.* extranormal3=0.0.3 ^
+"%CONDA%" create -y -n env python=3.6.* quasar=0.4.0 ^
+ orange3=3.21.0 orange-spectroscopy=0.4.4 opusFC=1.2.* ^
  keyring scipy=1.1.* numpy=1.15.* pyqt=5.6.* ^
  scikit-learn=0.20.* spectral ^
  colorcet h5py || exit /b !ERRORLEVEL!
