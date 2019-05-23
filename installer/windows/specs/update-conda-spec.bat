707873f7e9b5ae319c51bd5b4ea85fc544cadc45
Marko Toplak <marko.toplak@gmail.com>
2019-05-23 09:22:23 +0200

    Use scipy 1.2.*

--- installer/windows/specs/update-conda-spec.bat
+++ installer/windows/specs/update-conda-spec.bat
@@ -11,7 +11,7 @@ rem %CONDA% remove -y -n orange --all
 
 "%CONDA%" create -y -n env python=3.6.* quasar=0.4.0 ^
  orange3=3.21.0 orange-spectroscopy=0.4.4 opusFC=1.2.* ^
- keyring scipy=1.1.* numpy=1.15.* pyqt=5.6.* ^
+ keyring scipy=1.2.* numpy=1.15.* pyqt=5.6.* ^
  scikit-learn=0.20.* spectral ^
  colorcet h5py || exit /b !ERRORLEVEL!
 "%CONDA%" list -n env --export --explicit --md5 > env-spec.txt
