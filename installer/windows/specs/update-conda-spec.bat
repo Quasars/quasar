59ca6a410fc74c7324a96a7b4dcdfdc61bb9c45b
Marko Toplak <marko.toplak@gmail.com>
2019-06-27 19:15:40 +0200

    Release 0.5.0
    Orange 3.22.0, Orange-Spectroscopy 0.4.5

--- installer/windows/specs/update-conda-spec.bat
+++ installer/windows/specs/update-conda-spec.bat
@@ -9,9 +9,9 @@ if "%CONDA%" == "" (
 
 rem %CONDA% remove -y -n orange --all
 
-"%CONDA%" create -y -n env python=3.6.* quasar=0.4.0 ^
- orange3=3.21.0 orange-spectroscopy=0.4.4 opusFC=1.2.* ^
- keyring scipy=1.2.* numpy=1.15.* pyqt=5.6.* ^
- scikit-learn=0.20.* spectral ^
+"%CONDA%" create -y -n env python=3.6.* quasar=0.5.0 ^
+ orange3=3.22.0 orange-spectroscopy=0.4.5 opusFC=1.2.* ^
+ keyring scipy=1.2.* numpy=1.16.* pyqt=5.9.* ^
+ scikit-learn=0.21.* ^
  colorcet h5py || exit /b !ERRORLEVEL!
 "%CONDA%" list -n env --export --explicit --md5 > env-spec.txt
