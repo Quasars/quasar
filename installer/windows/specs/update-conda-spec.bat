b70dfe950f322f31eacbad6ea0570e0cd851b859
Marko Toplak <marko.toplak@gmail.com>
2019-12-23 13:28:28 +0100

    quasar 0.7.0

--- installer/windows/specs/update-conda-spec.bat
+++ installer/windows/specs/update-conda-spec.bat
@@ -9,9 +9,9 @@ if "%CONDA%" == "" (
 
 rem %CONDA% remove -y -n orange --all
 
-"%CONDA%" create -y -n env python=3.6.* quasar=0.6.0 ^
- orange3=3.23.0 orange-spectroscopy=0.4.6 opusFC=1.2.* ^
- keyring scipy=1.2.* numpy=1.16.* pyqt=5.9.* ^
- scikit-learn=0.21.* ^
+"%CONDA%" create -y -n env python=3.6.* quasar=0.7.0 ^
+ orange3=3.24.0 orange-spectroscopy=0.4.9 opusFC=1.2.* ^
+ keyring scipy=1.2.* numpy=1.16.* pyqt=5.12.* ^
+ scikit-learn=0.22.* ^
  colorcet h5py || exit /b !ERRORLEVEL!
 "%CONDA%" list -n env --export --explicit --md5 > env-spec.txt
