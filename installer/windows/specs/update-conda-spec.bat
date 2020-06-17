2a06233c722616d03e72c4a996b30b00fb1dd145
Marko Toplak <marko.toplak@gmail.com>
2020-06-17 13:55:12 +0200

    Release 0.9.0

--- installer/windows/specs/update-conda-spec.bat
+++ installer/windows/specs/update-conda-spec.bat
@@ -9,8 +9,8 @@ if "%CONDA%" == "" (
 
 rem %CONDA% remove -y -n orange --all
 
-"%CONDA%" create -y -n env python=3.6.* quasar=0.8.0 ^
- orange3=3.25.0 orange-spectroscopy=0.5.2 opusFC=1.2.* ^
+"%CONDA%" create -y -n env python=3.6.* quasar=0.9.0 ^
+ orange3=3.26.0 orange-spectroscopy=0.5.4 opusFC=1.2.* ^
  keyring scipy=1.2.* numpy=1.16.* pyqt=5.12.* ^
  scikit-learn=0.22.* ^
  colorcet h5py || exit /b !ERRORLEVEL!
