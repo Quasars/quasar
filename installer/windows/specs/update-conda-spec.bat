481f45d02707dbfd60ff7b709c7683131603e9cb
Marko Toplak <marko.toplak@gmail.com>
2020-06-18 11:45:47 +0200

    Upgrade installers to Python 3.7
    Conda parameter to only use tar.bz2 apparantly needs it.

--- installer/windows/specs/update-conda-spec.bat
+++ installer/windows/specs/update-conda-spec.bat
@@ -9,7 +9,7 @@ if "%CONDA%" == "" (
 
 rem %CONDA% remove -y -n orange --all
 
-"%CONDA%" create -y -n env python=3.6.* quasar=0.9.0 ^
+"%CONDA%" create -y -n env python=3.7.* quasar=0.9.0 ^
  orange3=3.26.0 orange-spectroscopy=0.5.4 opusFC=1.2.* ^
  keyring scipy=1.2.* numpy=1.16.* pyqt=5.12.* ^
  scikit-learn=0.22.* ^
