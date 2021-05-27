863f474ab3dae4d088d5876816d1d9de288977ef
Primož Godec <p.godec9@gmail.com>
2021-05-27 15:32:26 +0200

    Updated requirements

--- ci/appveyor-conda-build.bat
+++ ci/appveyor-conda-build.bat
@@ -42,7 +42,7 @@ if "%CONDA_SPEC_FILE%" == "" (
 
     "%CONDA%" create -n env --yes --use-local ^
                  python=%PYTHON_VERSION% ^
-                 numpy=1.16.* ^
+                 numpy=1.19.* ^
                  scipy=1.5.* ^
                  scikit-learn=0.23.* ^
                  bottleneck=1.3.* ^
