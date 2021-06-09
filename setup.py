a74892eae254765c6fedd4f34e4bccf2fd6ef5f3
Marko Toplak <marko.toplak@gmail.com>
2021-06-10 00:17:26 +0200

    Quasar 1.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages, Command
 
 NAME = "Quasar"
 
-VERSION = "1.1.0"
+VERSION = "1.2.0"
 
 DESCRIPTION = "Quasar is a collection of data analysis toolboxes extending the Orange suite."
 LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README.pypi')).read()
