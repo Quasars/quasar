0b97c77fe106599f941b8b3234ef22eb1b39c159
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2021-12-01 13:06:27 +0100

    condainstall.bat: Use call to invoke conda
    ... in case it is a .bat file with an exit statement.

--- scripts/windows/condainstall.bat
+++ scripts/windows/condainstall.bat
@@ -18,10 +18,10 @@ if not exist "%PREFIX%\python.exe" (
     echo Creating a conda env in "%PREFIX%"
     rem # Create an empty initial skeleton to layout the conda, activate.bat
     rem # and other things needed to manage the environment.
-    "%CONDA%" create --yes --prefix "%PREFIX%" --file conda-spec.txt ^
+    call "%CONDA%" create --yes --prefix "%PREFIX%" --file conda-spec.txt ^
         || exit /b !ERRORLEVEL!
 ) else (
-    "%CONDA%" install --yes --prefix "%PREFIX%" --file conda-spec.txt ^
+    call "%CONDA%" install --yes --prefix "%PREFIX%" --file conda-spec.txt ^
         || exit /b !ERRORLEVEL!
 )
 
