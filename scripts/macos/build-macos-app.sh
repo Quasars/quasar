biolab/orange3-installers

build-macos-app.sh: Don't check for installed PyQt5

scripts/macos/build-macos-app.sh
@@ -146,6 +146,6 @@ rm "${APPDIR}"/Contents/Info.plist.in
     cleanup() { rm -r "${tempdir}"; }
     trap cleanup EXIT
     cd "${tempdir}"
-    "${PYTHON}" -m pip install --no-cache-dir --no-index orange3 PyQt5
+    "${PYTHON}" -m pip install --no-cache-dir --no-index orange3
     "${PYTHON}" -m Orange.canvas --help > /dev/null
 )

Quasars/quasar

scripts/macos/build-macos-app.sh
@@ -146,6 +146,6 @@ rm "${APPDIR}"/Contents/Info.plist.in
     cleanup() { rm -r "${tempdir}"; }
     trap cleanup EXIT
     cd "${tempdir}"
-    "${PYTHON}" -m pip install --no-cache-dir --no-index quasar PyQt5
+    "${PYTHON}" -m pip install --no-cache-dir --no-index quasar
     "${PYTHON}" -m quasar --help > /dev/null
 )