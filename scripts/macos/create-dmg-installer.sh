de39555c92cb331350841446d9a4df04d68bda94
PrimozGodec <p.godec9@gmail.com>
2023-10-04 15:14:31 +0200

    create-dmg-installer - Replace hardcoded Orange3 app name [skip appveyor][skip azurepipelines]

--- scripts/macos/create-dmg-installer.sh
+++ scripts/macos/create-dmg-installer.sh
@@ -57,8 +57,12 @@ echo "============================================="
 
 mkdir -p "${TMP_TEMPLATE}"/
 
+BASENAME=$(basename $APP)
+echo "Copying the app ${BASENAME}"
+
 # Copy the .app directory in place
-cp -a "${APP}" "${TMP_TEMPLATE}"/Quasar.app
+cp -a "${APP}" "${TMP_TEMPLATE}"/"${BASENAME}"
+
 mkdir -p "$(dirname "${DMG}")"
 
 EXTRA_DS_STORE=()
@@ -78,8 +82,8 @@ create-dmg \
   --window-size 400 244 \
   --icon-size 75 \
   --text-size 12 \
-  --hide-extension "Quasar.app" \
-  --icon "Quasar.app" 95 125 \
+  --hide-extension "${BASENAME}" \
+  --icon "${BASENAME}" 95 125 \
   --app-drop-link 305 125 \
   ${EXTRA_DS_STORE[*]} \
   "${DMG}" \
