biolab/orange3-installers

Add -P flag to applications startup commands

scripts/windows/build-win-portable.sh
@@ -99,20 +99,20 @@ BUILDDIR_WIN="$(cygpath -w "${BUILDDIR}")"
 python -m pip install pywin32
 python "${DIR}/create_shortcut.py" \
    --target 'cmd.exe' \
-   --arguments '"/C start pythonw.exe -m Orange.canvas"' \
+   --arguments '"/C start pythonw.exe -Pm Orange.canvas"' \
    --working-directory "" \
    --window-style Minimized \
    --shortcut "${BUILDDIR_WIN}\Orange.lnk"
 
 python "${DIR}/create_shortcut.py" \
    --target 'cmd.exe' \
-   --arguments '"/K python.exe -m Orange.canvas -l4"' \
+   --arguments '"/K python.exe -Pm Orange.canvas -l4"' \
    --working-directory "" \
    --shortcut "${BUILDDIR_WIN}/Orange Debug.lnk"
 
 python "${DIR}/create_shortcut.py" \
    --target 'cmd.exe' \
-   --arguments '"/C start Orange\pythonw.exe -m Orange.canvas"' \
+   --arguments '"/C start Orange\pythonw.exe -Pm Orange.canvas"' \
    --working-directory "" \
    --window-style Minimized \
    --shortcut "${BUILDDIR_WIN}/../Orange.lnk"

Quasars/quasar

scripts/windows/build-win-portable.sh
@@ -99,20 +99,20 @@ BUILDDIR_WIN="$(cygpath -w "${BUILDDIR}")"
 python -m pip install pywin32
 python "${DIR}/create_shortcut.py" \
    --target 'cmd.exe' \
-   --arguments '"/C start pythonw.exe -m Orange.canvas"' \
+   --arguments '"/C start pythonw.exe -Psm Orange.canvas"' \
    --working-directory "" \
    --window-style Minimized \
    --shortcut "${BUILDDIR_WIN}\Orange.lnk"
 
 python "${DIR}/create_shortcut.py" \
    --target 'cmd.exe' \
-   --arguments '"/K python.exe -m Orange.canvas -l4"' \
+   --arguments '"/K python.exe -Psm Orange.canvas -l4"' \
    --working-directory "" \
    --shortcut "${BUILDDIR_WIN}/Orange Debug.lnk"
 
 python "${DIR}/create_shortcut.py" \
    --target 'cmd.exe' \
-   --arguments '"/C start Orange\pythonw.exe -m Orange.canvas"' \
+   --arguments '"/C start Orange\pythonw.exe -Psm Orange.canvas"' \
    --working-directory "" \
    --window-style Minimized \
    --shortcut "${BUILDDIR_WIN}/../Orange.lnk"